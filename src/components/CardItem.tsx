10e45dbd62d0eff96ca9063875de0f34b41e6929
JoshJAL <JoshuaALevine3@gmail.com>
2026-03-22 02:40:46 -0400

    Edit src/components/CardItem.tsx

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -41,7 +41,7 @@ export function CardItem({ card, onCheckToggle, done, onWorkOnThis, isSessionRun
           {card.name}
         </h3>
         <div className="flex items-center gap-2">
-          {!done && onWorkOnThis && (
+          {!done && onWorkOnThis && hasIncompleteTask && (
             <button
               onClick={() => onWorkOnThis(card)}
               disabled={isSessionRunning}
